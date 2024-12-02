e82fff995ef850213cbcf94e2635a5886caa30e8
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-12-02 23:57:27 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import Registration from './pages/Registration';
 import HomePage  from './pages/Home';
+import Navbar from "./pages/navbar"
 import Login from './pages/Login';
 import ApplyLoan from './pages/ApplyLoan';
 import Dashboard from './pages/Dashboard';
 function App() {
   return (
-    <div className="App">
       <Router>
+    <div className="App">
+      <Navbar></Navbar>
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/register" element={<Registration/>}></Route>
@@ -16,9 +18,9 @@ function App() {
           <Route path="/apply-loan" element={<ApplyLoan/>}></Route>
           <Route path="/dashboard" element={<Dashboard/>}></Route>
         </Routes>
-      </Router>
       {/* <h3 className="font-extrabold bg-red-600 text-yellow-600">hello there</h3> */}
     </div>
+      </Router>
   );
 }
 
---
+++ src/pages/navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar=()=>{
+    return(
+        <div className="nav">
+            <div className="image">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjvMYR9SR0gxupa9LY7BFB1mMctNd9g2Pcdg&s" alt=""/>
+            </div>
+            <ul className="list">
+            <li ><Link to="/register">Register</Link></li>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/apply-loan">Apply Loan</Link></li>
+            </ul>
+        </div>
+    )
+}
+export default Navbar;
