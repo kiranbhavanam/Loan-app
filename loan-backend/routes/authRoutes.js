4628d3a26484924036c142144b58b7df1d0aeeb3
kiran22022002 <54470122+kiran22022002@users.noreply.github.com>
2024-12-19 00:41:17 +0530

    Routing for apply loan

--- loan-backend/routes/authRoutes.js
+++ loan-backend/routes/authRoutes.js
@@ -1,12 +1,15 @@
 const express=require("express");
 
-const {register,login,getAllUsers}=require("../controllers/authController");
+const {register,login,getAllUsers,applyLoan,getLoans}=require("../controllers/authController");
 const {authenticate}=require("../middlewares/authMiddleware");
 
 const router=express.Router();
 router.post("/register",register)
 router.post("/login",login);
+router.post("/apply-loan",authenticate,applyLoan)
 router.get("/users",authenticate,getAllUsers);
+router.get("/loans",authenticate,getLoans)
+
 // router.get("/loans",authenticate,getLoans)
 
 module.exports=router;
